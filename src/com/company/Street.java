271263d5f83bb0f76f88e89612dd2dd8434e8645
Dimo <dimo_marino@abv.bg>
2020-02-24 22:19:01 +0200

    streets-up1

--- src/com/company/Street.java
+++ src/com/company/Street.java
@@ -2,16 +2,49 @@ package com.company;
 
 public class Street {
 
-    Street(int xCoordinate, int yCoordinate, String nameOfStreet, String color){
+    Street(int xCoordinate, int yCoordinate, String nameOfStreet, String color, int[] rent, int priceOfStreet){
         x = xCoordinate;
         y = yCoordinate;
         name = nameOfStreet;
+        price = priceOfStreet;
+        mortgage = priceOfStreet / 2;
+        this.color = color;
+        initHousePrice(color);
+        initRent(rent);
     }
 
-    int x;
-    int y;
-    String name;
-    String color;
-    String ownedBy;
+    public int x;
+    public int y;
+    public int price;
+    public int mortgage;
+    public int housePrice;
+    public int[] rent = new int[6];
+    public String name;
+    public String color;
+    public String ownedBy;
+
+    private void initHousePrice(String col){
+        switch (color){
+            case "brown":
+            case "blueLight":
+                housePrice = 50;
+                break;
+            case "pink":
+            case "orange":
+                housePrice = 100;
+                break;
+            case "red":
+            case "yellow":
+                housePrice = 150;
+                break;
+            case "green":
+            case "blueDark":
+                housePrice = 200;
+                break;
+        }
+    }
+    private void initRent(int[] rent){
+        for (int i = 0; i < rent.length; i++) this.rent[i] = rent[i];
+    }
 
 }
